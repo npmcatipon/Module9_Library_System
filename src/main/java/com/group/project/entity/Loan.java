eefbe79c92b910022f0fcb8352aff9a0a8f89f15
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-21 11:25:19 +0800

    Include constructor, getter and setter on Loan entity.

--- src/main/java/com/group/project/entity/Loan.java
+++ src/main/java/com/group/project/entity/Loan.java
@@ -26,4 +26,38 @@ public class Loan {
 
     @OneToMany(mappedBy = "loan")
     private List<Book> books;
+
+    public Loan() {
+    }
+
+    public Loan(Long id, User users, List<Book> books) {
+        this.id = id;
+        this.users = users;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUsers() {
+        return users;
+    }
+
+    public void setUsers(User users) {
+        this.users = users;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
 }
