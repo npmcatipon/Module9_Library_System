a91e1feb063bf1a7ee8435ad82e0d3bfbc311537
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-30 03:00:30 +0800

    added show all and borrowed books for BookRepository; added findByUserName in UesrRepository

--- src/main/java/com/group/project/repository/BookRepository.java
+++ src/main/java/com/group/project/repository/BookRepository.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.group.project.entity.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    @Query("SELECT b.id, b.title, b.author FROM Book b")
+    List<Book> showAllBooks();
 
     @Query("SELECT b.id, b.title, b.author FROM Book b WHERE b.borrowed = false")
     List<Book> showAvailableBooks();
+
+    @Query("SELECT b.id, b.title, b.author FROM Book b WHERE b.borrowed = true")
+    List<Book> showBorrowedBooks();
 }
--- src/main/java/com/group/project/repository/UserRepository.java
+++ src/main/java/com/group/project/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.group.project.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.group.project.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByUsername(String username);
 
 }
