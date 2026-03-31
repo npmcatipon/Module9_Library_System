46e76454cde7d07a32241857f9e5f48f1343cd9f
npmcatipon <npmcatipon@bpi.com.ph>
2026-03-31 09:21:37 +0800

    feat: add,update and delete book; convert from entity to DTO

--- src/main/java/com/group/project/service/BookService.java
+++ src/main/java/com/group/project/service/BookService.java
@@ -6,32 +6,84 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.group.project.repository.BookRepository;
+
+import jakarta.validation.Valid;
+
+import com.group.project.dto.BookDTO;
 import com.group.project.entity.Book;
+import com.group.project.mapper.BookMapper;
 
 @Service
 public class BookService {
 
 	private final BookRepository bookRepository;
+	private final BookMapper bookMapper;
 
 	List<Book> books = new ArrayList<>();
 
-	public BookService(BookRepository bookRepository) {
+	public BookService(BookRepository bookRepository,
+		BookMapper bookMapper) {
 		this.bookRepository = bookRepository;
+		this.bookMapper = bookMapper;
 	}
 
-	public List<Book> getAllBooks() {
-		return bookRepository.showAllBooks();
+	public List<BookDTO> getAllBooks() {
+		return bookRepository
+				.showAllBooks()
+			   	.stream()
+			   	.map(bookMapper::toDto)
+			   	.toList();
 	}
 
-	public List<Book> getAvailableBooks() {
-		return bookRepository.showAvailableBooks();
+	public List<BookDTO> getAvailableBooks() {
+		return bookRepository
+				.showAvailableBooks()
+				.stream()
+				.map(bookMapper::toDto)
+				.toList();
 	}
 
-	public List<Book> getBorrowedBooks() {
-		return bookRepository.showBorrowedBooks();
+	public List<BookDTO> getBorrowedBooks() {
+		return bookRepository
+				.showBorrowedBooks()
+				.stream()
+				.map(bookMapper::toDto)
+				.toList();
 	}
 
-	public List<Book> getById(Long id) {
-		return bookRepository.findById(id).stream().toList();
+	public BookDTO getById(Long id) {
+		Book book = bookRepository
+					.findById(id)
+					.orElseThrow(() -> new RuntimeException("Book not found."));
+		return bookMapper.toDto(book);
 	}
+
+	public BookDTO create(@Valid BookDTO bookDTO) {
+		Book newBook = bookRepository.save(bookMapper.toEntity(bookDTO));
+		BookDTO newBookDTO = bookMapper.toDto(newBook);
+		return newBookDTO;
+	}
+
+	public BookDTO update(Long id, @Valid BookDTO updatedBook) {
+		Book book = bookRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Invalid Book ID"));
+		
+		book.setTitle(updatedBook.getTitle());
+		book.setAuthor(updatedBook.getAuthor());
+		book.setBorrowed(updatedBook.getBorrowed());
+
+		Book savedBook = bookRepository.save(book);
+
+		return bookMapper.toDto(savedBook);
+	}
+
+	public BookDTO delete(Long id) {
+		Book book = bookRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("Book ID not found"));
+
+		bookRepository.delete(book);
+
+		return bookMapper.toDto(book);
+	}
+
 }
