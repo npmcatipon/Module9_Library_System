cb696903806b227b9dbd2ce14810eb0b360d2620
npmcatipon <npmcatipon@bpi.com.ph>
2026-04-03 05:15:23 +0800

    removed unused import

--- src/main/java/com/group/project/controller/BookController.java
+++ src/main/java/com/group/project/controller/BookController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.group.project.dto.BookDTO;
-import com.group.project.dto.UserDTO;
 import com.group.project.service.BookService;
 
 import jakarta.validation.Valid;
